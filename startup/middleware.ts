7db173bede26274e9c79a59e6f61596ff868785a
Elvis <elvissamir18@gmail.com>
2022-03-28 19:01:56 -0400

    Added middleware: helmet, cors, express.json, compression.

---
+++ startup/middleware.ts
@@ -0,0 +1,12 @@
+import express from "express"
+import { Express } from "express"
+import helmet from "helmet";
+import cors from "express"
+import compression from "compression"
+
+export default function (app: Express) {
+    app.use(helmet)
+    app.use(cors)
+    app.use(express.json())
+    app.use(compression)
+}
